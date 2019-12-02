ea104f17e1b8d17f57c17f6d535f653aafbb3e74
K00232599-Kevin-Dunne <55739514+K00232599-Kevin-Dunne@users.noreply.github.com>
2019-12-02 11:39:54 +0000

    update

--- Code/Website/Eclipse Version/PathFinder/src/AddNewMapActionDB.java
+++ Code/Website/Eclipse Version/PathFinder/src/AddNewMapActionDB.java
@@ -13,173 +13,175 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @MultipartConfig(maxFileSize = 16177216) // upto 16 MB
-@WebServlet(name = "AddNewMapActionDB", urlPatterns = {"/AddNewMapActionDB"})
-public class AddNewMapActionDB extends HttpServlet 
-{
-    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-    Connection conn;
-    Connection conn2;
-    Statement stmt;
-    PreparedStatement prepStat;
+@WebServlet(name = "AddNewMapActionDB", urlPatterns = { "/AddNewMapActionDB" })
+public class AddNewMapActionDB extends HttpServlet {
+	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+	Connection conn;
+	Connection conn2;
+	Statement stmt;
+	PreparedStatement prepStat;
 
-    int powerID;
-    String powerUsername;
-    String powerFirstName;
-    String powerLastName;
-    String powerPassword;
-    String powerEmail;
-    int powerStatus;
-    ResultSet result;
+	int powerID;
+	String powerUsername;
+	String powerFirstName;
+	String powerLastName;
+	String powerPassword;
+	String powerEmail;
+	int powerStatus;
+	ResultSet result;
 
-    int idRights;
-    String userNameRights;
-    String passwordRights;
-    String emailRights;
-    int AccountStatusRights;
-    int special;
-    String userOrgRights;
-    
-    String organisation_name;
-    String organisation_address;
-    String organisation_email;
-    String organisation_mobile;
-    String organisation_building_name;
-    String user_org_name;
-    
-    String map_location_url;
-    String important_points;
-    String map_comments;
-    Blob map_image_floor;
-    String org_building_name;
+	int idRights;
+	String userNameRights;
+	String passwordRights;
+	String emailRights;
+	int AccountStatusRights;
+	int special;
+	String userOrgRights;
 
-    @Override
-    public void init() throws ServletException {
-        String URL = "jdbc:mysql://remotemysql.com:3306/4eyg55o51S?autoReconnect=true&useSSL=false";
-        String USERNAME = "4eyg55o51S";
-        String PASSWORD = "ADRFyeBfRn";
+	String organisation_name;
+	String organisation_address;
+	String organisation_email;
+	String organisation_mobile;
+	String organisation_building_name;
+	String user_org_name;
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            // setup the connection with the DB
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Connected");
-        } catch (ClassNotFoundException | SQLException e) {
-            System.err.println("Error 1: " + e);
-        }
-    }
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // create a cookie array
-    	getPower(request);
-        map_location_url = request.getParameter("important_points");
-        important_points = request.getParameter("important_points");
-        map_comments = request.getParameter("map_comments");
-        //map_image_floor = request.getParameter("map_image_floor");
-        org_building_name = request.getParameter("org_building_name");
-        InputStream inputStream = null;
-        Part parts1 = request.getPart("map_image_floor");
-        inputStream = parts1.getInputStream();
-        try {
-            prepStat = conn.prepareStatement("insert into maps values(? ,? ,? ,? ,? ,?");
-            prepStat.setInt(1, 0);
-            prepStat.setString(2, map_location_url);
-            prepStat.setString(3, important_points);
-            prepStat.setString(4, map_comments);
-            prepStat.setBlob(5, inputStream);
-            prepStat.setString(6, org_building_name);
-            
-            int i = prepStat.executeUpdate();
-            response.sendRedirect("orgDB");
-} catch (SQLException ex) {
-    Logger.getLogger(ControlDB.class.getName()).log(Level.SEVERE, null, ex);
-    //response.sendRedirect("UploadMapDB");
-    System.err.println("inputStream: " + inputStream);
-    System.err.println("Error 2: " + ex);
-    
-}
-       
-    }
-    public void getPower(HttpServletRequest request)
-    {
-        Cookie cookie = null;
-        Cookie[] cookies = null;
-        cookies = request.getCookies();
-        // were going to loop through the cookies array and if the active cookies match
-        // values in the database
-        // we know we are that user in the database so were going to put there
-        // information into some variables
-        try {
-            //
-            if (cookies != null) {
-                for (int i = 0; i < cookies.length; i++) {
-                    cookie = cookies[i];
-                    stmt = conn.createStatement();
-                    String sql5 = "select user_id,user_name,password,account_rank_account_rank_id,email,organisation_name from users";
-                    result = stmt.executeQuery(sql5);
-                    while (result.next()) {
-                    	String user_org_name = result.getString("organisation_name");
-                        String powerUsername = result.getString("user_name");
-                        int powerID = result.getInt("user_id");
-                        String powerPassword = result.getString("password");
-                        int powerStatus = result.getInt("account_rank_account_rank_id");
-                        String powerEmail = result.getString("email");
+	String map_location_url;
+	String important_points;
+	String map_comments;
+	Blob map_image_floor;
+	String org_building_name;
 
-                        // if cookie username and username from the database match then we are this
-                        // record,
-                        // extremly important note!: all usernames are unique so they database cannot
-                        // contain 2 exact usernames
-                        if (powerUsername.equals(cookie.getValue())) {
-                            userNameRights = powerUsername;
-                            idRights = powerID;
-                            passwordRights = powerPassword;
-                            AccountStatusRights = powerStatus;
-                            emailRights = powerEmail;
-                            userOrgRights = user_org_name;
-                        }
-                    }
-                }
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(ControlDB.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	@Override
+	public void init() throws ServletException {
+		String URL = "jdbc:mysql://remotemysql.com:3306/4eyg55o51S?autoReconnect=true&useSSL=false";
+		String USERNAME = "4eyg55o51S";
+		String PASSWORD = "ADRFyeBfRn";
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// setup the connection with the DB
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			System.out.println("Connected");
+		} catch (ClassNotFoundException | SQLException e) {
+			System.err.println("Error 1: " + e);
+		}
+	}
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// create a cookie array
+		getPower(request);
+		map_location_url = request.getParameter("important_points");
+		important_points = request.getParameter("important_points");
+		map_comments = request.getParameter("map_comments");
+		// map_image_floor = request.getParameter("map_image_floor");
+		org_building_name = request.getParameter("org_building_name");
+		InputStream inputStream = null;
+		Part parts1 = request.getPart("map_image_floor");
+		inputStream = parts1.getInputStream();
+		try {
+			prepStat = conn.prepareStatement("insert into maps values(? ,? ,? ,? ,? ,?");
+			prepStat.setInt(1, 0);
+			prepStat.setString(2, map_location_url);
+			prepStat.setString(3, important_points);
+			prepStat.setString(4, map_comments);
+			prepStat.setBlob(5, inputStream);
+			prepStat.setString(6, org_building_name);
+
+			int i = prepStat.executeUpdate();
+			//response.sendRedirect("orgDB");
+		} catch (SQLException ex) {
+			Logger.getLogger(ControlDB.class.getName()).log(Level.SEVERE, null, ex);
+			// response.sendRedirect("UploadMapDB");
+			System.err.println("inputStream: " + inputStream);
+			System.err.println("Error 5: "+parts1.getInputStream());
+			System.err.println("Error 6: "+parts1);
+			System.err.println("Error 2: " + ex);
+
+		}
+	}
+
+	public void getPower(HttpServletRequest request) {
+		Cookie cookie = null;
+		Cookie[] cookies = null;
+		cookies = request.getCookies();
+		// were going to loop through the cookies array and if the active cookies match
+		// values in the database
+		// we know we are that user in the database so were going to put there
+		// information into some variables
+		try {
+			//
+			if (cookies != null) {
+				for (int i = 0; i < cookies.length; i++) {
+					cookie = cookies[i];
+					stmt = conn.createStatement();
+					String sql5 = "select user_id,user_name,password,account_rank_account_rank_id,email,organisation_name from users";
+					result = stmt.executeQuery(sql5);
+					while (result.next()) {
+						String user_org_name = result.getString("organisation_name");
+						String powerUsername = result.getString("user_name");
+						int powerID = result.getInt("user_id");
+						String powerPassword = result.getString("password");
+						int powerStatus = result.getInt("account_rank_account_rank_id");
+						String powerEmail = result.getString("email");
+
+						// if cookie username and username from the database match then we are this
+						// record,
+						// extremly important note!: all usernames are unique so they database cannot
+						// contain 2 exact usernames
+						if (powerUsername.equals(cookie.getValue())) {
+							userNameRights = powerUsername;
+							idRights = powerID;
+							passwordRights = powerPassword;
+							AccountStatusRights = powerStatus;
+							emailRights = powerEmail;
+							userOrgRights = user_org_name;
+						}
+					}
+				}
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(ControlDB.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
 // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException      if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException      if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
 
 }
