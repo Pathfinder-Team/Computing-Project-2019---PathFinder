001d8b30225a7cec256b57c9fbeda7fc0b1bccea
K00232599-Kevin-Dunne <55739514+K00232599-Kevin-Dunne@users.noreply.github.com>
2020-01-09 23:11:20 +0000

    update

--- Code/Android/Kate_ver/PathFinder/app/src/main/java/com/example/pathfinder/GetMapActivity.java
+++ Code/Android/Kate_ver/PathFinder/app/src/main/java/com/example/pathfinder/GetMapActivity.java
@@ -27,22 +27,16 @@ public class GetMapActivity extends AppCompatActivity {
     private String TAG = GetMapActivity.class.getSimpleName();
 
     private ProgressDialog pDialog;
-    private ListView lv;
     SQLiteDatabase db;
 
     // URL to get contacts JSON
     private static String url = "https://pathsearcher.azurewebsites.net/ActionJson";
 
-    ArrayList<HashMap<String, String>> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_map);
-
-        contactList = new ArrayList<>();
-
-        lv = (ListView) findViewById(R.id.list);
 
         new GetContacts().execute();
     }
@@ -52,6 +46,7 @@ public class GetMapActivity extends AppCompatActivity {
      */
     private class GetContacts extends AsyncTask<Void, Void, Void> {
 
+        /*
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -61,7 +56,7 @@ public class GetMapActivity extends AppCompatActivity {
             pDialog.setCancelable(false);
             pDialog.show();
 
-        }
+        }*/
 
         @Override
         protected Void doInBackground(Void... arg0) {
@@ -77,55 +72,53 @@ public class GetMapActivity extends AppCompatActivity {
                     JSONObject jsonObj = new JSONObject(jsonStr);
 
                     // Getting JSON Array node
-                    JSONArray contacts = jsonObj.getJSONArray("map_points");
+                    JSONArray PathFinderMap = jsonObj.getJSONArray("map_points");
+                    JSONArray PathFinderMap2 = jsonObj.getJSONArray("special_points");
 
-                    db=openOrCreateDatabase("EventDB", Context.MODE_PRIVATE,null);
+                    db=openOrCreateDatabase("mapDB", Context.MODE_PRIVATE,null);
                     db.execSQL("CREATE TABLE IF NOT EXISTS " +
-                            "map_points(current_point_id int ," +
+                            "map_points(" +
+                            "current_point_id int ," +
+                            "point_name varhar," +
+                            "maps_map_id int);");
+
+                    db.execSQL("CREATE TABLE IF NOT EXISTS " +
+                            "special_points(" +
+                            "point_id int ," +
                             "point_from_id int," +
                             "point_to_id int," +
                             "point_weight int," +
-                            "maps_map_id int," +
-                            "point_direction VARCHAR," +
-                            "point_name varhar);");
+                            "point_direction varhar);");
 
                     // looping through All Contacts
-                    for (int i = 0; i < contacts.length(); i++) {
-                        JSONObject c = contacts.getJSONObject(i);
+                    for (int i = 0; i < PathFinderMap.length(); i++) {
+                        JSONObject c = PathFinderMap.getJSONObject(i);
 
                         int current_point_id = c.getInt("current_point_id");
                         String point_name = c.getString("point_name");
                         int maps_map_id = c.getInt("maps_map_id");
-                        int point_from_id = c.getInt("point_from_id");
-                        int point_to_id = c.getInt("point_to_id");
-                        int point_weight = c.getInt("point_weight");
-                        String point_direction = c.getString("point_direction");
-
-
+                        System.out.println("Check: "+point_name);
                         db.execSQL("INSERT INTO map_points VALUES('"
                                 +current_point_id+"','"
+                                +point_name+"','"
+                                +maps_map_id+"');");
+                    }
+                    for (int j = 0; j < PathFinderMap.length(); j++) {
+
+                        JSONObject cc = PathFinderMap2.getJSONObject(j);
+
+                        int point_id = cc.getInt("point_id");
+                        int point_from_id = cc.getInt("point_from_id");
+                        int point_to_id = cc.getInt("point_to_id");
+                        int point_weight = cc.getInt("point_weight");
+                        String point_direction = cc.getString("point_direction");
+
+                        db.execSQL("INSERT INTO special_points VALUES('"
+                                +point_id+"','"
                                 +point_from_id+"','"
                                 +point_to_id+"','"
                                 +point_weight+"','"
-                                +maps_map_id+"','"
-                                +point_direction+"','"
-                                +point_name+"');");
-
-                        // tmp hash map for single contact
-                        HashMap<String, String> contact = new HashMap<>();
-
-                        // adding each child node to HashMap key => value
-                        //contact.put("current_point_id", current_point_id);
-
-                        contact.put("maps_map_id", Integer.toString(maps_map_id));
-                        contact.put("point_from_id", Integer.toString(point_from_id));
-                        contact.put("point_to_id", Integer.toString(point_to_id));
-                        contact.put("point_weight", Integer.toString(point_weight));
-                        contact.put("point_name", point_name);
-                        contact.put("point_direction", point_direction);
-
-                        // adding contact to contact list
-                        contactList.add(contact);
+                                +point_direction+"');");
                     }
                 } catch (final JSONException e) {
                     Log.e(TAG, "Json parsing error: " + e.getMessage());
@@ -153,33 +146,7 @@ public class GetMapActivity extends AppCompatActivity {
                 });
 
             }
-
             return null;
         }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            // Dismiss the progress dialog
-            if (pDialog.isShowing())
-                pDialog.dismiss();
-            /**
-             * Updating parsed JSON data into ListView
-             * */
-            ListAdapter adapter = new SimpleAdapter(
-                    GetMapActivity.this, contactList,
-                    R.layout.list_item, new String[]{"point_name",
-                    "point_from_id",
-                    "point_to_id",
-                    "point_weight",
-                    "point_direction"}, new int[]{R.id.point_name,
-                    R.id.point_from_id,
-                    R.id.point_to_id,
-                    R.id.point_weight,
-                    R.id.point_direction});
-
-            lv.setAdapter(adapter);
-        }
-
     }
 }
--- Code/Android/Kate_ver/PathFinder/app/src/main/java/com/example/pathfinder/PathFinder.java
+++ Code/Android/Kate_ver/PathFinder/app/src/main/java/com/example/pathfinder/PathFinder.java
@@ -2,6 +2,7 @@ package com.example.pathfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -13,12 +14,13 @@ import java.util.ArrayList;
 public class PathFinder extends AppCompatActivity {
     ArrayList<String> pointNames;
     SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_path_finder);
 
-
+        db=openOrCreateDatabase("mapDB", Context.MODE_PRIVATE,null);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getPointName());
 
         Spinner mSpinner = findViewById(R.id.spinner);
@@ -28,14 +30,17 @@ public class PathFinder extends AppCompatActivity {
     public ArrayList<String> getPointName()
     {
         // Cursor c = getReadableDatabase().rawQuery("SELECT * FROM friendinfotable",null);
+
         Cursor c = db.rawQuery("select * from map_points",null);
 
         StringBuffer buffer = new StringBuffer();
         while(c.moveToNext())
         {
-            System.out.println("Special"+c.getString(6));
+            System.out.println("Special: "+c.getString(1));
             pointNames.add(c.getString(6));
         }
+
+
         return pointNames;
     }
 }
