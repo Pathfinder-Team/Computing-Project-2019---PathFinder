3971640608b29dfa29852c665a2cf5471e8cb3c9
K00232599-Kevin-Dunne <55739514+K00232599-Kevin-Dunne@users.noreply.github.com>
2020-01-10 05:14:05 +0000

    update

--- Code/Android/Kate_ver/PathFinder/app/src/main/java/com/example/pathfinder/DisplayActivity.java
+++ Code/Android/Kate_ver/PathFinder/app/src/main/java/com/example/pathfinder/DisplayActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class DisplayActivity extends AppCompatActivity {
+public class DisplayActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     String current_selected = "";
@@ -21,8 +23,13 @@ public class DisplayActivity extends AppCompatActivity {
         Bundle extras = intent.getExtras();
         current_selected = extras.getString("current_selected");
         selected_destination = extras.getString("selected_destination");
-    }
 
+        System.out.println("current_selected: "+current_selected);
+        System.out.println("selected_destination:"+selected_destination);
+
+        Button btn_scan = (Button) findViewById(R.id.btn_scan);
+        btn_scan.setOnClickListener(this);
+    }
     protected void onStart()
     {
         super.onStart();
@@ -30,5 +37,16 @@ public class DisplayActivity extends AppCompatActivity {
         TextView mes2 =  (TextView)findViewById(R.id.display_next);
         mes1.setText(current_selected);
         mes2.setText(selected_destination);
+    }
+
+    public void onClick(View view)
+    {
+        Intent intent;
+        switch(view.getId()) {
+            case R.id.btn_scan:
+                intent = new Intent(this, ScanActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
